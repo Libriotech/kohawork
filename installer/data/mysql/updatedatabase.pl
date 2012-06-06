782a46e5b58e70d1990518a25d462703422b9100
Magnus Enger <magnus@enger.priv.no>
2012-06-06 22:33:11 +0200

    Fix updatedatabase.pl

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -5291,7 +5291,7 @@ if ( C4::Context->preference("Version") < TransformToNum($DBversion) ) {
     $dbh->do("INSERT INTO permissions (module_bit, code, description) VALUES (13, 'edit_shelf_maps', 'Add and edit shelf maps');");
     print "Upgrade to $DBversion done (Added permission for adding and editing shelfmaps)\n";
     SetVersion($DBversion);
-
+}
 
 
 =head1 FUNCTIONS
