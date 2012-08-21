afa1f561c387a7f563759527070df4ce57ed2e82
Magnus Enger <magnus@enger.priv.no>
2012-08-21 15:07:23 +0200

    Bug 8628 - Add the edit_digital_signs privilege

--- installer/data/mysql/atomicupdate/bug8628-digital-signs.sql
+++ installer/data/mysql/atomicupdate/bug8628-digital-signs.sql
@@ -1,3 +1,5 @@
 -- SQL related to Bug 8628 - Add digital signs to the OPAC
 
 INSERT INTO systempreferences (variable,value,explanation,type) VALUES ('OPACDigitalSigns',0,'Turn digital signs in the OPAC on or off.','YesNo');
+
+INSERT INTO permissions (module_bit, code, description) VALUES (13, 'edit_digital_signs', 'Create and edit digital signs for the OPAC');
