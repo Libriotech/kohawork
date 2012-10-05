c064869b40883df84dd295076ab4e525933eb85d
Magnus Enger <magnus@enger.priv.no>
2012-10-05 11:34:54 +0200

    Bug 8628 - Start rename process with bug8628-digital-signs.sql
    Renaming:
    Signs -> Streams
    Decks -> Signs

--- installer/data/mysql/atomicupdate/bug8628-digital-signs.sql
+++ installer/data/mysql/atomicupdate/bug8628-digital-signs.sql
@@ -25,30 +25,30 @@ INSERT INTO systempreferences (variable,value,explanation,type) VALUES ('OPACDig
 INSERT INTO permissions (module_bit, code, description) VALUES (13, 'edit_digital_signs', 'Create and edit digital signs for the OPAC');
 
 -- TODO Add these tables to kohasctructure.sql
+DROP TABLE IF EXISTS sign_streams;
+CREATE TABLE sign_streams (
+  sign_stream_id int(11) NOT NULL auto_increment,    -- primary key, used to identify streams
+  saved_sql_id int(11) NOT NULL,              -- foreign key from the saved_sql table
+  name varchar(32),                           -- name/title of the sign
+  PRIMARY KEY (sign_stream_id),
+  CONSTRAINT sign_streams_ibfk_1 FOREIGN KEY (saved_sql_id) REFERENCES saved_sql (id) ON DELETE CASCADE
+);
 DROP TABLE IF EXISTS signs;
 CREATE TABLE signs (
   sign_id int(11) NOT NULL auto_increment,    -- primary key, used to identify signs
-  saved_sql_id int(11) NOT NULL,              -- foreign key from the saved_sql table
-  name varchar(32),                           -- name/title of the sign
-  PRIMARY KEY (sign_id),
-  CONSTRAINT signs_ibfk_1 FOREIGN KEY (saved_sql_id) REFERENCES saved_sql (id) ON DELETE CASCADE
-);
-DROP TABLE IF EXISTS decks;
-CREATE TABLE decks (
-  deck_id int(11) NOT NULL auto_increment,    -- primary key, used to identify decks
   name varchar(32),                           -- name/title of the deck
   branchcode varchar(10) NOT NULL default '', -- foreign key from the branches table
   webapp int(1) NOT NULL default 0,           -- display as web app or normal page
-  PRIMARY KEY (deck_id),
-  CONSTRAINT decks_ibfk_1 FOREIGN KEY (branchcode) REFERENCES branches (branchcode) ON DELETE CASCADE
+  PRIMARY KEY (sign_id),
+  CONSTRAINT signs_ibfk_1 FOREIGN KEY (branchcode) REFERENCES branches (branchcode) -- ON DELETE CASCADE
 );
-DROP TABLE IF EXISTS signs_to_decks;
-CREATE TABLE signs_to_decks (
-  deck_id int(11) NOT NULL,    -- foreign key from the decks table
-  sign_id int(11) NOT NULL,    -- foreign key from the signs table
-  PRIMARY KEY (deck_id,sign_id),
-  CONSTRAINT signs_to_decks_sdfk_1 FOREIGN KEY (deck_id) REFERENCES decks (deck_id) ON DELETE CASCADE,
-  CONSTRAINT signs_to_decks_sdfk_2 FOREIGN KEY (sign_id) REFERENCES signs (sign_id) ON DELETE CASCADE
+DROP TABLE IF EXISTS signs_to_streams;
+CREATE TABLE signs_to_streams (
+  sign_stream_id int(11) NOT NULL, -- foreign key from the decks sign_streams table
+  sign_id int(11) NOT NULL,        -- foreign key from the signs table
+  PRIMARY KEY (sign_stream_id,sign_id),
+  CONSTRAINT signs_to_streams_ibfk_1 FOREIGN KEY (sign_stream_id) REFERENCES sign_streams (sign_stream_id) ON DELETE CASCADE,
+  CONSTRAINT signs_to_streams_ibfk_2 FOREIGN KEY (sign_id) REFERENCES signs (sign_id) ON DELETE CASCADE
 );
 
 
@@ -63,11 +63,11 @@ INSERT INTO saved_sql
 ( 3,  51,             '2012-08-22 12:55:33', '2012-08-22 12:55:33', 'SELECT biblionumber, title FROM biblio ORDER BY RAND() DESC LIMIT 20',                                  NULL,     'Random titles',    1,     NULL,  300,          1 ),
 ( 4,  51,             '2012-09-04 12:55:33', '2012-09-04 12:55:33', 'SELECT biblionumber, title FROM biblio WHERE title LIKE "%Perl%" OR title LIKE "%PHP%" ORDER BY RAND() DESC', NULL, 'Tech books',     1, NULL, 300,          1 );
 
-INSERT INTO decks ( deck_id, branchcode, name, webapp ) VALUES ( 1, 'CPL', 'Signs for the main library', 1 );
+INSERT INTO signs ( sign_id, branchcode, name, webapp ) VALUES ( 1, 'CPL', 'Signs for the main library', 1 );
 
-INSERT INTO signs ( sign_id, saved_sql_id, name ) VALUES ( 1, 1, 'Newest titles' );
-INSERT INTO signs ( sign_id, saved_sql_id, name ) VALUES ( 2, 2, 'Titles from 2012' );
-INSERT INTO signs ( sign_id, saved_sql_id, name ) VALUES ( 3, 3, 'Random titles' );
-INSERT INTO signs ( sign_id, saved_sql_id, name ) VALUES ( 4, 4, 'Tech books' );
+INSERT INTO sign_streams ( sign_stream_id, saved_sql_id, name ) VALUES ( 1, 1, 'Newest titles' );
+INSERT INTO sign_streams ( sign_stream_id, saved_sql_id, name ) VALUES ( 2, 2, 'Titles from 2012' );
+INSERT INTO sign_streams ( sign_stream_id, saved_sql_id, name ) VALUES ( 3, 3, 'Random titles' );
+INSERT INTO sign_streams ( sign_stream_id, saved_sql_id, name ) VALUES ( 4, 4, 'Tech books' );
 
-INSERT INTO signs_to_decks (deck_id, sign_id) VALUES (1,1),(1,2),(1,3),(1,4);
+INSERT INTO signs_to_streams (sign_id, sign_stream_id) VALUES (1,1),(1,2),(1,3),(1,4);
