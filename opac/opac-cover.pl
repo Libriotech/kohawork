8b2b6506de84b3a80c5654812be772242a83cce8
Magnus Enger <magnus@enger.priv.no>
2012-11-27 15:32:57 +0100

    Bug 8628 - Whitepsace and indent in opac-cover.pl

--- opac/opac-cover.pl
+++ opac/opac-cover.pl
@@ -37,62 +37,60 @@ my $debug = 0;
 my $query = new CGI;
 my $biblionumber = $query->param('biblionumber') || '' || $query->param('bib');
 
-my ( $template, $borrowernumber, $cookie ) = get_template_and_user(
-  {
+my ( $template, $borrowernumber, $cookie ) = get_template_and_user({
     template_name   => \"",
     query           => $query,
     type            => "opac",
     authnotrequired => ( C4::Context->preference("OpacPublic") ? 1 : 0 ),
-    flagsrequired => { borrow => 1 },
-  }
-);
+    flagsrequired   => { borrow => 1 },
+});
 
 if ( $biblionumber ne '' ) {
 
-  # TODO Check the cache
+    # TODO Check the cache
 
-  # TODO Check that OPACLocalCoverImages is activated
-  my @covers = ListImagesForBiblio( $biblionumber );
-  warn 'Local covers: ', Dumper @covers if $debug;
-  if ( $covers[0] ) {
-    # TODO Cache the result
-    my $url = '/cgi-bin/koha/opac-image.pl?thumbnail=' . $covers[0] . '&biblionumber=' . $biblionumber;
-    warn 'URL: ', $url if $debug;
-    print $query->redirect( $url );
-    exit;
-  }
+    # TODO Check that OPACLocalCoverImages is activated
+    my @covers = ListImagesForBiblio( $biblionumber );
+    warn 'Local covers: ', Dumper @covers if $debug;
+    if ( $covers[0] ) {
+        # TODO Cache the result
+        my $url = '/cgi-bin/koha/opac-image.pl?thumbnail=' . $covers[0] . '&biblionumber=' . $biblionumber;
+        warn 'URL: ', $url if $debug;
+        print $query->redirect( $url );
+        exit;
+    }
 
-  my $record = GetMarcBiblio( $biblionumber );
-  my $isbn   = GetNormalizedISBN( undef, $record, C4::Context->preference("marcflavor") ) || '';
+    my $record = GetMarcBiblio( $biblionumber );
+    my $isbn   = GetNormalizedISBN( undef, $record, C4::Context->preference("marcflavor") ) || '';
 
-  if ( $isbn ne '' ) {
-    warn "ISBN: $isbn" if $debug;
+    if ( $isbn ne '' ) {
+        warn "ISBN: $isbn" if $debug;
 
-    my $ua = LWP::UserAgent->new;
+        my $ua = LWP::UserAgent->new;
 
-    # Open Library
-    my $url = 'http://covers.openlibrary.org/b/isbn/' . $isbn . '-M.jpg';
+        # Open Library
+        my $url = 'http://covers.openlibrary.org/b/isbn/' . $isbn . '-M.jpg';
 
-    # Do a HEAD request and check the status of the response.
-    # Adding ?default=false to the URL makes the service return 404 when an
-    # image is not found instead of returning a blank 1x1 image
-    # http://openlibrary.org/dev/docs/api/covers
-    my $url2 .= '?default=false';
-    # We don't actually need this logic at the moment, though, since it is the
-    # last one we try
-    # my $response = $ua->head( $url2 );
-    # if ($response->is_success) {
-    #   # TODO Cache the result
-    #   print $query->redirect( $url2 );
-    #   exit;
-    # } else {
-    #   warn $response->status_line, " for ", $url2 if $debug;
-    # }
+        # Do a HEAD request and check the status of the response.
+        # Adding ?default=false to the URL makes the service return 404 when an
+        # image is not found instead of returning a blank 1x1 image
+        # http://openlibrary.org/dev/docs/api/covers
+        my $url2 .= '?default=false';
+        # We don't actually need this logic at the moment, though, since it is the
+        # last one we try
+        # my $response = $ua->head( $url2 );
+        # if ($response->is_success) {
+        #   # TODO Cache the result
+        #   print $query->redirect( $url2 );
+        #   exit;
+        # } else {
+        #   warn $response->status_line, " for ", $url2 if $debug;
+        # }
 
-    print $query->redirect( $url );
-  }
+        print $query->redirect( $url );
+    }
 
-  # FIXME What to do when there is no ISBN?
-  print $query->redirect( 'http://div.libriotech.no/files/2012/nicole.jpg' );
+    # FIXME What to do when there is no ISBN?
+    print $query->redirect( 'http://div.libriotech.no/files/2012/nicole.jpg' );
 
 }
