346992ae2ce9a19bba162b64ed8a534d5fbb1a74
Magnus Enger <magnus@libriotech.no>
2015-07-19 12:17:28 +0200

    Make opac-nncipp.pl send ItemRequested
    Needs some more polishing, but the basic functionality is there.

---
+++ Koha/ILLRequest/Backend/NNCIPP.pm
@@ -0,0 +1,114 @@
+package Koha::ILLRequest::Backend::NNCIPP;
+
+# Copyright Magnus Enger Libriotech 2015
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with Koha; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+use C4::Members::Attributes qw ( GetBorrowerAttributeValue );
+use C4::Log;
+use HTTP::Tiny;
+use Modern::Perl;
+use Data::Dumper; # FIXME Debug
+use base qw(Exporter);
+
+our @EXPORT_OK = qw( 
+    send_ItemRequested
+);
+
+=head1 NAME
+
+Koha::ILLRequest::Backend::NNCIPP - Koha ILL Backend: NNCIPP (Norwegian NCIP Profile)
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+A first stub file to implement NNCIPP for Koha. 
+
+=head1 SUBROUTINES
+
+=head2 send_ItemRequested 
+
+Typically triggered when a library has logged into the OPAC and placed an ILL
+request there. This message is sent back to the ILS of the library that made 
+the request, so that they know what they have requested. 
+
+=cut
+
+sub send_ItemRequested {
+
+    my ( $request_id, $borrower ) = @_;
+    
+    my $nncip_uri = GetBorrowerAttributeValue( $borrower->borrowernumber, 'nncip_uri' );
+
+    my $msg = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>
+<ns1:NCIPMessage ns1:version=\"http://www.niso.org/schemas/ncip/v2_02/ncip_v2_02.xsd\" xmlns:ns1=\"http://www.niso.org/2008/ncip\">
+    <ns1:ItemRequested>
+        <ns1:InitiationHeader>
+            <ns1:FromAgencyId>
+                <ns1:AgencyId>NO-" . $borrower->cardnumber . "</ns1:AgencyId>
+            </ns1:FromAgencyId>
+            <ns1:ToAgencyId>
+                <ns1:AgencyId>CPL</ns1:AgencyId>
+            </ns1:ToAgencyId>
+        </ns1:InitiationHeader>
+        <ns1:UserId>
+            <ns1:UserIdentifierValue>51</ns1:UserIdentifierValue>
+        </ns1:UserId>
+        <ns1:ItemId>
+            <ns1:ItemIdentifierValue>1</ns1:ItemIdentifierValue>
+        </ns1:ItemId>
+        <ns1:RequestType>Loan</ns1:RequestType>
+        <ns1:RequestScopeType>0</ns1:RequestScopeType>
+    </ns1:ItemRequested>
+</ns1:NCIPMessage>";
+
+    _send_message( $msg, $nncip_uri );
+
+}
+
+=head1 INTERNAL SUBROUTINES
+
+=head2 _send_message
+
+Do the actual sending of XML messages to NCIP endpoints.
+
+=cut
+
+sub _send_message {
+
+    my ( $msg, $endpoint ) = @_;
+    
+    warn "endpoint: $endpoint";
+    
+    my $http = HTTP::Tiny->new();
+    my $response = $http->post( 'http://koha.ncip.bibkat.no:3000', { 'content' => $msg } );
+    
+    if ( $response->{success} ){
+        return { 'success' => 1, 'msg' => $response->{'content'} };
+    } else {
+        return { 'success' => 0, 'msg' => "$response->{status} $response->{reason}" };
+    }
+
+}
+
+=head1 AUTHOR
+
+Magnus Enger <magnus@libriotech.no>
+
+=cut
+
+1;
