178a3d69cd5c373178cf89bb3a4e1f5c4ad2cbf3
Magnus Enger <magnus@libriotech.no>
2017-05-19 10:32:33 +0200

    Fix sysprefs

---
+++ installer/data/mysql/atomicupdate/semantikoha.sql
@@ -0,0 +1,2 @@
+-- TODO Add these to sysprefs.sql
+INSERT INTO systempreferences SET variable = 'OPACDetailViewLinkedData', value = 0, options = NULL, explanation = "Show or hide Linked Data in the OPAC detail view.", type = 'YesNo';
