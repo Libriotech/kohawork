fccb4501d0929fb2ba5d24e2d10cef104158cb04
Magnus Enger <magnus@libriotech.no>
2017-05-23 21:08:10 +0200

    Update the module

--- Koha/LinkedData.pm
+++ Koha/LinkedData.pm
@@ -17,6 +17,12 @@ package Koha::LinkedData;
 # with Koha; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
+use Modern::Perl;
+use C4::Context;
+
+# TODO The code in here should probably be moved to Koha::RDF or thereabouts,
+# but I'll keep it as a separate module for now, to avoid conflicts.
+
 =head1 NAME
 
 Koha::LinkedData
@@ -31,22 +37,51 @@ Koha::LinkedData
 
 =cut
 
-use Modern::Perl;
-use Carp;
-use C4::Context;
-use base qw(Class::Accessor);
+sub new {
+    my ($class, $args) = @_;
+    $args = {} unless defined $args;
+    return bless ($args, $class);
+}
 
 sub get_data_from_biblionumber {
 
     my ( $self, $biblionumber ) = @_;
 
+    # Mint the URI
+    # my $rdf = Koha::RDF->new;
+    # my $uri = $rdf->mint_uri('biblio',1);
+    # For now, we fake it, so we can get some actual data from the triplestore
+    my $uri = 'http://demo.semweb.bibkat.no/bib/1';
+
     my $triplestore = C4::Context->triplestore;
-    my $sparql = '
-    SELECT * WHERE { ?s ?p ?o }
-    ';
+    my $sparql = "
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX bibframe: <http://id.loc.gov/ontologies/bibframe/>
+  SELECT ?type1 ?type2 ?type3 WHERE  { {
+    <$uri> rdfs:seeAlso ?graph
+    FILTER ( ?type1 != '' )
+  } UNION {
+    GRAPH ?graph {
+      ?graph rdf:type ?type1 .
+      ?graph <http://schema.org/mainEntity> ?mainEnt .
+      ?mainEnt rdf:type ?type2 .
+      ?mainEnt bibframe:instanceOf ?instanceOf .
+      ?instanceOf rdf:type ?type3 .
+    }
+} }
+";
     my $data = $triplestore->get_sparql( $sparql );
 
-    my $tt = '<ul>[% WHILE ( d = ld_data.next ) %]<li>* [% d.o.value %]</li>[% END %]</ul>';
+    my $tt = '
+<ul>
+[% WHILE ( d = ld_data.next ) %]
+  <li>[% d.type1.value %]</li>
+  <li>[% d.type2.value %]</li>
+  <li>[% d.type3.value %]</li>
+[% END %]
+</ul>
+';
 
     return ( $data, $tt );
 
@@ -59,7 +94,7 @@ None by default.
 
 =head1 AUTHOR
 
-Chris Cormack, E<lt>chris@bigballofwax.co.nzE<gt>
+Magnus Enger, E<lt>magnus@libriotech.noE<gt>
 
 =cut
 
